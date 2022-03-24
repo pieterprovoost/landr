969bcf87eff42502d511274805dbdd69a7d74b12
Pieter Provoost <pieterprovoost@gmail.com>
2022-03-25 00:33:35 +0100

    added simplified option

--- R/landr.R
+++ R/landr.R
@@ -7,7 +7,7 @@ cache_env <- new.env(parent = emptyenv())
 #'
 #' @docType package
 #' @name landr
-#' @import ggplot2 sf memoise
+#' @import ggplot2 sf memoise glue
 #' @author Pieter Provoost, \email{pieterprovoost@gmail.com}
 NULL
 
@@ -15,12 +15,12 @@ NULL
 #'
 #' @param after index of the layer after which the land polygons should be added. Set to 0 to add at the bottom.
 #' @export
-geom_landr <- function(after = 0, ...) {
-  structure(list(after = after, args = list(...)), class = "landr")
+geom_landr <- function(after = 0, simplified = FALSE, ...) {
+  structure(list(after = after, simplified = simplified, args = list(...)), class = "landr")
 }
 
-get_clip <- function(bbox, crs) {
-  st_crop(st_transform(get_land_polygons(), crs = crs), bbox)
+get_clip <- function(bbox, crs, simplified) {
+  st_crop(st_transform(get_land_polygons(simplified), crs = crs), bbox)
 }
 
 get_clip_cached <- memoise::memoise(get_clip)
@@ -35,7 +35,7 @@ ggplot_add.landr <- function(object, plot, object_name) {
   dy <- yrange[2] - yrange[1]
   bbox <- c(xmin = xrange[1] - dx * buffer, xmax = xrange[2] + dx * buffer, ymin = yrange[1] - dy * buffer, ymax = yrange[2] + dy * buffer)
   crs <- ifelse(is.null(b$layout$coord$crs), 4326, b$layout$coord$crs)
-  clip <- get_clip_cached(bbox, crs)
+  clip <- get_clip_cached(bbox, crs, object$simplified)
   new_coord <- plot$coordinates
   new_coord$limits$x <- xrange
   new_coord$limits$y <- yrange
@@ -49,15 +49,23 @@ ggplot_add.landr <- function(object, plot, object_name) {
 #' Load land polygons from OpenStreetMap.
 #'
 #' @usage get_land_polygons()
+#' @param simplified return simplified polygons.
 #' @export
-get_land_polygons <- function() {
+get_land_polygons <- function(simplified = FALSE) {
+  if (simplified == TRUE) {
+    folder_name <- "simplified-land-polygons-complete-3857"
+    file_name <- "simplified_land_polygons.shp"
+  } else {
+    folder_name <- "land-polygons-complete-4326"
+    file_name <- "land_polygons.shp"
+  }
   if (is.null(cache_env$land_polygons)) {
     extdata_path <- system.file("extdata", package = "landr")
-    polygons_shapefile <- file.path(extdata_path, "land-polygons-complete-4326", "land_polygons.shp")
+    polygons_shapefile <- file.path(extdata_path, folder_name, file_name)
     if (!file.exists(polygons_shapefile)) {
-      polygons_archive <- file.path(extdata_path, "land-polygons-complete-4326.zip")
+      polygons_archive <- file.path(extdata_path, glue("{folder_name}.zip"))
       message("Downloading shapefile from OpenStreetMap")
-      download.file("https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip", polygons_archive)
+      download.file(glue("https://osmdata.openstreetmap.de/download/{folder_name}.zip"), polygons_archive)
       unzip(polygons_archive, exdir = extdata_path)
     }
     message("Loading shapefile")
